746c36647c1340d0c10dd2de081392cd3d871ee2
unknown <cristian.daniel.herrera@hotmail.com>
2021-12-30 22:04:11 +0000

    3commit

--- src/Components/HomeBody/HomeBodyStyles.js
+++ src/Components/HomeBody/HomeBodyStyles.js
@@ -4,19 +4,13 @@ export const Body = styled.div`
 width: 100%;
 height: 150vh;
 position: absolute;
-top: 10%;
-
+top: 5%;
 background-image: url('./assets/techdigi3.jpg'); 
 background-color: black;
-
-
 `;
 
 export const Loading = styled.div`
 display: ${(props) => (props.load ? "block" : "none")};
-
-
-
 `
 
 export const BodyContainer = styled.div`
@@ -29,7 +23,6 @@ top: 5%;
 right: 0;
 left: 0;
 flex-wrap: wrap-reverse;
-
 @media screen and (max-width: 900px) {
     
     top: 20%;
@@ -38,9 +31,6 @@ flex-wrap: wrap-reverse;
     
     top: 10%;
   }
-
-
-
 `
 
 // Box 1
@@ -59,13 +49,11 @@ display: ${(props)=> (props.load ? 'none' : 'block' ) };
 `;
 
 export const ComputerContainer = styled.div`
-
 width: 95%;
 height: auto;
 /* border: solid 1px white; */
 position: relative;
 z-index: 10;
-
 `;
 
 export const Computer = styled.img`
@@ -73,8 +61,6 @@ width: 95%;
 filter: invert(0.9);
 z-index: 20;
 position: relative;
-
-
 `
 
 export const TextComputer= styled.div`
@@ -91,7 +77,6 @@ padding: 3%;
 `
 export const Parrafo = styled.p`
 border-right: ${(props)=> (props.tapping ? '' : 'none' )} ;
-
 color: #abf40b;
 `
 
@@ -102,7 +87,6 @@ position: absolute;
 background-color: black;
 top: 62%;
 right: 18%;
-
 `
 
 // Box 2
@@ -113,7 +97,7 @@ align-items: center;
 flex-direction: column;
 width: 50%;
 height: auto;
-border: solid 1px white;
+
 min-width: 265px;
 @media screen and (max-width: 600px) {
     width: 100%;
@@ -121,29 +105,22 @@ min-width: 265px;
   @media screen and (max-width: 900px) {
     width: 100%;
   }
-
-
 `;
 
 export const HiThereDiv = styled.div`
 width: 90%;
 margin-bottom: 10%;
-
-
 `
 
 export const HiThereH3 = styled.h3`
 text-align: center;
 color: #fff;
 font-size: 150%;
-
 `
 
 export const HireMeDiv = styled.div`
 width: auto;
-
 text-align: center;
-
 `
 
 export const HireMeButton = styled.button`
@@ -151,16 +128,13 @@ border: none;
 background-color: transparent;
 color: #fff;
 cursor: pointer;
-
 background: #500779;
 border-radius: 5px;
 padding: 8px 3px;
-
-
+font-size: 150%;
 &:hover , &:focus {
     color: #ffffff;
     
-
       box-shadow: 0 0 8px #0e54ea, 0 0 2px #0e54ea, 0 0 10px #0e54ea,
       0 0 6px #0e54ea, 0 0 20px #0e54ea, 0 0 20px #0e54ea, 0 0 1px #0e54ea,
       0 0 12px #0e54ea;
@@ -168,16 +142,3 @@ padding: 8px 3px;
   
   }
 `
-
-
-
-
-
-
-
-
-
-
-
-
-
